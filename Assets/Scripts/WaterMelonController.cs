15142eb0a745cbc4b11de7b874de660246cb5694
Bjarke  Larsen <bjarke@larsen-dk.dk>
2016-04-23 15:38:28 +0200

    watermelon splitting works now

--- Assets/Scripts/WaterMelonController.cs
+++ Assets/Scripts/WaterMelonController.cs
@@ -6,6 +6,7 @@ public class WaterMelonController : MonoBehaviour {
 	public GameObject m_melonPrefab;
 	public int m_level = 0;
 	public static int MaxLevel = 1;
+	public float splitVelocity = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +21,39 @@ public class WaterMelonController : MonoBehaviour {
 	}
 
 	void Split(int currentLevel) {
-		if (currentLevel == MaxLevel)
-			return;
+		if (currentLevel < MaxLevel){
+			GameObject splitMelonLeft = (GameObject)Instantiate(m_melonPrefab);
+			splitMelonLeft.GetComponent<WaterMelonController>().m_level = currentLevel + 1;
+			splitMelonLeft.transform.localScale = transform.localScale / 2;
+			splitMelonLeft.transform.localPosition = splitMelonLeft.transform.position + new Vector3(transform.localScale.x,0,0);
+			splitMelonLeft.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
 
-		GameObject splitMelon = (GameObject)Instantiate(m_melonPrefab);
-		splitMelon.GetComponent<WaterMelonController>().m_level = currentLevel + 1;
-		splitMelon.transform.localScale = transform.localScale / 2;
+			GameObject splitMelonRight = (GameObject)Instantiate(m_melonPrefab);
+			splitMelonRight.GetComponent<WaterMelonController>().m_level = currentLevel + 1;
+			splitMelonRight.transform.localScale = transform.localScale / 2;
+			splitMelonRight.transform.position = splitMelonRight.transform.position + new Vector3(-transform.localScale.x,0,0);
+			splitMelonRight.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
+
+		}
+
+
+
+
+		Destroy(gameObject);
 	}
+
+
+	void OnCollisionEnter2D(Collision2D c){
+		if(c.gameObject.tag == "Watermelon"){
+			return;
+		}
+
+		Rigidbody2D rbOther = c.gameObject.GetComponent<Rigidbody2D>();
+		if(rbOther != null){
+			if((rbOther.mass+rbOther.velocity.sqrMagnitude) >= splitVelocity){
+				Split(m_level);
+			}
+		}
+	}
+
 }
