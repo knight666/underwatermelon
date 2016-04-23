e62fae6626dea99d3af0702f998ab529513dddb9
Jacob L. Olsen <jacoblynggaardolsen@hotmail.com>
2016-04-23 15:32:09 +0200

    working on bubbles

---
+++ Assets/Juice/BubbleParticles/bubblesOnVelocity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bubblesOnVelocity : MonoBehaviour {
+
+	public static float defaultAmount;
+	private GameObject ball;
+	private Rigidbody2D ballRb;
+	private ParticleSystem particles;
+	private 
+
+	void OnEnable(){
+		if (ball == null) {
+			ball = gameObject;
+		}
+		if (particles == null) {
+			particles = GetComponent<ParticleSystem> ();
+		}
+		if (ball != null) {
+			ballRb = ball.GetComponent<Rigidbody2D> ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float rbVel = ballRb.velocity.magnitude;
+		ParticleSystem.EmissionModule em = particles.emission;
+		ParticleSystem.MinMaxCurve mm = new ParticleSystem.MinMaxCurve ();
+		mm.constantMax = defaultAmount + rbVel;
+		em.rate = mm;
+	}
+}
