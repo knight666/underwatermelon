97c0e5e01622f92201cb7b649ea04e035d155d72
Stefanie Enge <stefanie.enge89@googlemail.com>
2016-04-23 15:20:48 +0200

    adds scorecards and a goal counter for two teams

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalManager : MonoBehaviour {
+
+	public int scoreValue = 1; //Score added to the player after goal
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+//		if (Input.GetKeyDown (KeyCode.Space))		just for testing the Scorecards
+//			scoring ("one");
+//		if (Input.GetKeyDown (KeyCode.Return))
+//			scoring ("two");
+	}
+
+	void scoring () {
+		
+		if (this.gameObject.name == "goal1") {
+			ScoreManager.score2 += scoreValue;
+		}
+		if (this.gameObject.name == "goal2") {
+			ScoreManager.score1 += scoreValue;
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "watermelon") {
+
+			scoring ();
+			Destroy (col.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score1;		// Scorenumber Team 1
+	public static int score2;		// Scorenumber Team 2
+	Text scoreText1;  				// Score Text Team 1
+	Text scoreText2;  				// Score Text Team 1
+
+	void Awake ()
+	{
+		score1 = 0;
+		score2 = 0;
+		scoreText1 = GameObject.Find("Text1").GetComponent <Text> ();
+		scoreText2 = GameObject.Find("Text2").GetComponent <Text> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		scoreText1.text = "Score: " + score1;
+		scoreText2.text = "Score: " + score2;
+	}
+}
