be57feb511dce4227a854fb3b11710366d9ad588
David Schornsheim <david.schornsheim@gmx.us>
2016-04-23 19:02:47 +0200

    Fixing missing scripts

---
+++ Assets/Scripts/Audio/RoundRobinClip.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Assets.Scripts.Audio;
+
+namespace Assets.Scripts.Audio 
+{
+	[Serializable] 
+	public class RoundRobinClip 
+	{
+		public RoundRobinComponent roundRobinComponent = null;
+		public float minRand;
+		public float maxRand;
+		public float minDry = -500;
+		public float maxDry = 0;
+		public bool inUse = false;
+	}
+}
---
+++ Assets/Scripts/Audio/RoundRobinComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoundRobinComponent : MonoBehaviour {
+    
+	public int lastIndex = -1; // The index of the last triggered sound
+	[SerializeField][HideInInspector]public List<AudioClip> roundRobins = new List<AudioClip> ();
+
+	void Awake() {
+		lastIndex = -1;
+	}
+
+	void OnDestroy() {
+		lastIndex = -1;
+	}
+	void OnApplicationQuit() {
+		lastIndex = -1;
+	}
+	void Reset() {
+		lastIndex = -1;
+	}
+	void OnDisable() {
+		lastIndex = -1;
+	}
+}
