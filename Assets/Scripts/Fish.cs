849e705da8e41c804c0819e95a0f2bd38055c38a
Bjarke  Larsen <bjarke@larsen-dk.dk>
2016-04-23 16:26:50 +0200

    reset game!

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public Vector3 startPosition;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	public void Reset(){
+		transform.position = startPosition;
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = 0.0f;
+		transform.rotation = Quaternion.identity;
+	}
+
+}
